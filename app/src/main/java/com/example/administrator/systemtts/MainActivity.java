0c1bc2565708a9e8b117275674534aa96838c2ae
wuzhiqiang <wuzhiqiang@qeeniao.com>
2018-12-26 15:27:09 +0800

    由于之前代码是随手摘过来的，故先进行了一个简化

--- app/src/main/java/com/example/administrator/systemtts/MainActivity.java
+++ app/src/main/java/com/example/administrator/systemtts/MainActivity.java
@@ -2,14 +2,63 @@ package com.example.administrator.systemtts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etWords, etPitch, etSpeech;
+    private Button btnPlay;
+    private SystemTTS systemTTS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SystemTTS systemTTS = SystemTTS.getInstance(getApplicationContext());
-        systemTTS.playText("你好,世界，我是小白");
+
+        initView();
+
+        initData();
+
+        initListener();
+    }
+
+    private void initView() {
+        etWords = (EditText) findViewById(R.id.et_word);
+        etPitch = (EditText) findViewById(R.id.et_pitch);
+        etSpeech = (EditText) findViewById(R.id.et_speech);
+        btnPlay = (Button) findViewById(R.id.btn_play);
+    }
+
+
+    private void initData() {
+        systemTTS = SystemTTS.getInstance(this);
+    }
+
+
+    private void initListener() {
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String words = getWords();
+                if (!TextUtils.isEmpty(words)) {
+                    systemTTS.play(words);
+                }
+            }
+        });
+    }
+
+
+    private String getWords() {
+        return etWords.getText().toString().trim();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        systemTTS.destroy();
     }
 }
--- app/src/main/java/com/example/administrator/systemtts/SystemTTS.java
+++ app/src/main/java/com/example/administrator/systemtts/SystemTTS.java
@@ -1,22 +1,36 @@
 package com.example.administrator.systemtts;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.speech.tts.TextToSpeech;
-import android.speech.tts.TextToSpeech.OnUtteranceCompletedListener;
-import android.speech.tts.UtteranceProgressListener;
+import android.widget.Toast;
 
 import java.util.Locale;
 
 /**
- * 系统播报类,部分手机不支持中文播报
+ * 语音播放的一个单例对象
  */
-@SuppressLint("NewApi")
-public class SystemTTS extends UtteranceProgressListener implements TTS, OnUtteranceCompletedListener {
-    private Context mContext;
+public class SystemTTS {
+
+    //单例对象
     private static SystemTTS singleton;
-    private TextToSpeech textToSpeech; // 系统语音播报类
-    private boolean isSuccess = true;
+    //context对象
+    private Context mContext;
+    //核心播放对象
+    private TextToSpeech textToSpeech;
+    //是否支持
+    private boolean isSupport = true;
+
+    private SystemTTS(Context context) {
+        this.mContext = context.getApplicationContext();
+        textToSpeech = new TextToSpeech(mContext, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                //textToSpeech的配置
+                init(i);
+            }
+        });
+    }
+
 
     public static SystemTTS getInstance(Context context) {
         if (singleton == null) {
@@ -29,86 +43,45 @@ public class SystemTTS extends UtteranceProgressListener implements TTS, OnUtter
         return singleton;
     }
 
-    private SystemTTS(Context context) {
-        this.mContext = context.getApplicationContext();
-        textToSpeech = new TextToSpeech(mContext, new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int i) {
-                //系统语音初始化成功
-                if (i == TextToSpeech.SUCCESS) {
-                    int result = textToSpeech.setLanguage(Locale.CHINA);
-                    textToSpeech.setPitch(1.0f);// 设置音调，值越大声音越尖（女生），值越小则变成男声,1.0是常规
-                    textToSpeech.setSpeechRate(1.0f);
-                    textToSpeech.setOnUtteranceProgressListener(SystemTTS.this);
-                    textToSpeech.setOnUtteranceCompletedListener(SystemTTS.this);
-                    if (result == TextToSpeech.LANG_MISSING_DATA
-                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                        //系统不支持中文播报
-                        isSuccess = false;
-                    }
-                }
-
+    //textToSpeech的配置
+    private void init(int i) {
+        if (i == TextToSpeech.SUCCESS) {
+            int result = textToSpeech.setLanguage(Locale.ENGLISH);
+            // 设置音调，值越大声音越尖（女生），值越小则变成男声,1.0是常规
+            textToSpeech.setPitch(2.0f);
+            textToSpeech.setSpeechRate(3.0f);
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                //系统不支持中文播报
+                isSupport = false;
             }
-        });
-    }
-
-
-    public void destroy() {
-        stopSpeak();
-        if (textToSpeech != null) {
-            textToSpeech.shutdown();
         }
-        singleton = null;
     }
 
-    public void init() {
-    }
 
-    public void playText(String playText) {
-        if (!isSuccess) {
+    public void play(String text) {
+        if (!isSupport) {
+            Toast.makeText(mContext, "暂不支持", Toast.LENGTH_SHORT).show();
             return;
         }
         if (textToSpeech != null) {
-            textToSpeech.speak(playText,
-                    TextToSpeech.QUEUE_ADD, null, null);
+            textToSpeech.speak(text, TextToSpeech.QUEUE_ADD, null, null);
         }
     }
 
-    public void stopSpeak() {
+
+    public void stop() {
         if (textToSpeech != null) {
             textToSpeech.stop();
         }
     }
 
-    @Override
-    public boolean isPlaying() {
-        return textToSpeech.isSpeaking();
-    }
 
-    ICallBack callBack = null;
-
-    @Override
-    public void setCallback(ICallBack callback) {
-        callBack = callback;
+    public void destroy() {
+        stop();
+        if (textToSpeech != null) {
+            textToSpeech.shutdown();
+        }
     }
 
 
-    //播报完成回调
-    @Override
-    public void onUtteranceCompleted(String utteranceId) {
-    }
-
-    @Override
-    public void onStart(String utteranceId) {
-
-    }
-
-    @Override
-    public void onDone(String utteranceId) {
-    }
-
-    @Override
-    public void onError(String utteranceId) {
-
-    }
 }
